13115dfdebc5dea88eb53b9550256229fa854cb4
361754100@qq.com <13751823091.jian>
2019-01-03 16:59:20 +0800

    Initial commit

---
+++ src/main/java/com/hro/core/ckafka/CkafkaApplication.java
@@ -0,0 +1,14 @@
+package com.hro.core.ckafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkafkaApplication.class, args);
+	}
+
+}
+
